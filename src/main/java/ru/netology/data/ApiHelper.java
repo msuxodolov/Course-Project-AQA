87b209979bb2b86c5d0150d93e5c087216924cfa
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-04-21 16:17:10 +0300

    change src/test/java/ApiTest.java

--- src/main/java/ru/netology/data/ApiHelper.java
+++ src/main/java/ru/netology/data/ApiHelper.java
@@ -2,12 +2,39 @@ package ru.netology.data;
 
 import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ApiHelper {
 
+    private static QueryRunner runner = new QueryRunner();
+    private ApiHelper() {
+    }
 
+    private static Connection getConn() throws SQLException {
+        return DriverManager.getConnection("db",  "app", "qwer123");
+    }
+
+    @SneakyThrows
+    public static String getStatusSQL() {
+        Thread.sleep(10000);
+        var payStatus = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        var conn = getConn();
+        var result = runner.query(conn, payStatus, new ScalarHandler<String>());
+        return result;
+    }
+
+    @SneakyThrows
+    public static int getAmountSQL() {
+        Thread.sleep(10000);
+        var amount = "SELECT amount FROM payment_entity order by created desc LIMIT 1";
+        var conn = getConn();
+        var result = runner.query(conn, amount, new ScalarHandler<Integer>());
+        return result;
+
+    }
 
 }
